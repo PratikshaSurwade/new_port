8a09ada9ee2f173988d16f339ecb94af054dd32a
PratikshaSurwade <89350917+PratikshaSurwade@users.noreply.github.com>
2025-06-28 14:33:44 +0530

    Backend complete

--- backend/lib/connectDb.js
+++ backend/lib/connectDb.js
@@ -3,8 +3,8 @@ import  mongoose from 'mongoose';
 export const connectDb = async () => {
     try {
         await mongoose.connect(process.env.MONGOURL);
-        console.log("db connected successfylly")
+        console.log("db connected successfully");
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
